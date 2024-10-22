7751976b816c7d64f134002a32d43213a81234f0
MoustachedMamba <gekuznetsof@yandex.ru>
2024-10-23 00:48:29 +0300

    More assets from ashan pizhenniy 2020 build
    - Аномальные штуки
    - Земельные штуки

--- Scenes/FSM/PlayerController/Looking.gd
+++ Scenes/FSM/PlayerController/Looking.gd
@@ -1,10 +1,13 @@
 class_name Looking extends PCState
 
 
+@export var looking_time: float = 1.3
+
 func enter(previous_state_path: String, data := {}) -> void:
 	var tween := create_tween()
-	tween.tween_property(player.progress_bar, "value", 100, 1.848)
+	tween.tween_property(player.progress_bar, "value", 100, looking_time)
 	await tween.finished
+
 	player.progress_bar.value = 0
 	await player.step_timer.timeout
 	state_machine._transition_to_next_state(MOVING)
