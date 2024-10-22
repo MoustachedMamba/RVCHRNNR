f195f4cbc399194da2fda6638ccc6be59eca2757
MoustachedMamba <gekuznetsof@yandex.ru>
2024-10-22 19:25:29 +0300

    Update Looking.gd
    - More sync

--- Scenes/FSM/PlayerController/Looking.gd
+++ Scenes/FSM/PlayerController/Looking.gd
@@ -6,6 +6,7 @@ func enter(previous_state_path: String, data := {}) -> void:
 	tween.tween_property(player.progress_bar, "value", 100, 1.848)
 	await tween.finished
 	player.progress_bar.value = 0
+	await player.step_timer.timeout
 	state_machine._transition_to_next_state(MOVING)
 
 func exit() -> void:
